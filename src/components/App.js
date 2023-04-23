6b7629ebdde13b61f9c6ed3318109efdb07a09ba
Anushka Verma <anushkaverma779@gmail.com>
2023-04-23 19:00:52 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,13 @@
+import React from "react";
+import Tabs from "./Tabs";
+import "../styles/App.css";
 
+const App = () => {
+    return (
+        <>
+            <Tabs />
+        </>
+    );
+}
+
+export default App;
